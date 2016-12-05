fe11546f628309539a429ea31503fc2dcdd7ea2b
Praarthi <praarthana.r@gmail.com>
2016-12-04 20:07:45 -0500

    removed commneted lines

--- Project6/startCode/main.cpp
+++ Project6/startCode/main.cpp
@@ -1,53 +1,28 @@
 #include <iostream>
-//#include "parse.tab.hpp"
 
-//extern FILE * yyin;
-//extern int keywords;
-//extern int identifiers;
-//extern int numbers;
-//extern int yydebug;
 extern int yylineno;
-
 int yylex(void);
 extern int yyparse();
 
-/*void openFile(const char *filename) {
-    FILE *file = fopen(filename, "r");
-    if (!file) {
-      std::cerr << "Could not open file \"" 
-                << filename 
-                << "\"" << std::endl;
-      exit(1);
-    }
-    yyin = file;
-}
-*/
 void printInfo(void)
-{ std::cout << std::endl;
- // std::cout << "keywords: " << keywords << std::endl;
- // std::cout << "identifiers: " << identifiers << std::endl;
- // std::cout << "numbers: " << numbers << std::endl;
-  std::cout << "lines: " << yylineno << std::endl;
+{ 
+    std::cout << std::endl;
+    std::cout << "lines: " << yylineno << std::endl;
 }
 
 int main(int argc, char * argv[]) {
- /* if (argc > 1) { // user-supplied filename
-    openFile(argv[1]);
-  }*/
-// yydebug = 0;
 
-  int parseErrors = yyparse();
-  if ( parseErrors ) {
-    std::cerr << "Abnormal termination" << std::endl;
-    return 1;
-  }
-  //printInfo();
-  return 0;
+    int parseErrors = yyparse();
+    if ( parseErrors ) {
+        std::cerr << "Abnormal termination" << std::endl;
+        return 1;
+    }
+    return 0;
 }
 
 int oldMain() {
-  yylex();
-  printInfo();
-  return 0;
+    yylex();
+    printInfo();
+    return 0;
 }
 
